186efdcb29a06402dbd260ba68b8542a177eba6a
Claudio Giordano <claudio.giordano@autistici.org>
2023-12-07 22:26:16 +0100

    Initial analysis script

---
+++ analyze.mjs
@@ -0,0 +1,10 @@
+import fs from 'fs';
+
+const data = JSON.parse(fs.readFileSync('./data/achievements.json'));
+console.log(data.length)
+const permAchis = data.filter(ach => ach.flags.includes('Permanent'));
+let acc = 0;
+//const permAchisPts = permAchis.reduce((acc, value) => console.log(value));
+console.log('perm achis', permAchis.length);
+//console.log('perm achis pts', permAchisPts);
+// console.log(achis)
