76689607ca9c18f78eb6a2483ce7fb0e6a09880a
Claudio Giordano <claudio.giordano@autistici.org>
2023-12-08 04:45:20 +0100

    Refactor api calls to optimize fetch and data store

--- script/export_achievements.mjs
+++ script/export_achievements.mjs
@@ -2,12 +2,36 @@
 
 import fs from 'fs';
 import {setTimeout as sleep} from 'timers/promises';
+import cliProgress from 'cli-progress'
+const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 const baseURL = 'https://api.guildwars2.com/v2';
 const reqConfig = {
 	headers: {
 		'Content-Type': 'application/json'
 	}
+};
+
+const getAchievementsGroups = async () => {
+	const resp = await fetch(`${baseURL}/achievements/groups`, reqConfig);
+	return await resp.json();
+};
+
+const getAchievementsGroupById = async (groupId) => {
+	const resp = await fetch(`${baseURL}/achievements/groups/${groupId}`, reqConfig);
+	return await resp.json();
+};
+
+const getAchievementsCategories = async () => {
+	process.stdout.write('Fetching achievemnts categories...');
+	const resp = await fetch(`${baseURL}/achievements/categories`, reqConfig);
+	process.stdout.write(' done!\n');
+	return await resp.json();
+};
+
+const getAchievementsCategoryById = async (cId) => {
+	const resp = await fetch(`${baseURL}/achievements/categories?ids=${cId}`, reqConfig);
+	return await resp.json();
 };
 
 const getAchievementsIds = async () => {
@@ -18,23 +42,36 @@ const getAchievementsIds = async () => {
 };
 
 const getAchievementById = async aId => {
-	//console.info(`Fetching achievemnt ${aId}...`);
-	const resp = await fetch(`${baseURL}/achievements/${aId}`, reqConfig);
-	//console.info(`Fetching achievemnt ${aId} completed`);
+	const resp = await fetch(`${baseURL}/achievements?ids=${aId}`, reqConfig);
 	return await resp.json();
 };
 
-let ids = await getAchievementsIds();
-ids = ids.sort((a, b) => a - b);
-// console.log(ids);
-let achievements = [];
-let counter = 1;
-for (let idx in ids) {
-	achievements.push(await getAchievementById(ids[idx]));
-	process.stdout.write(`\rFetching ${counter} / ${ids.length}`);
-	counter++;
-	await sleep(150);
+const achievements = await getAchievementsGroups();
+// console.log('achievements', achievements.length, achievements);
+
+bar.start(achievements.length, 0);
+for (const idx in achievements) {
+	/**
+	 * Populate groups
+	 */
+	achievements[idx] = await getAchievementsGroupById(achievements[idx]);
+
+	/**
+	 * Populate categories
+	 */
+	achievements[idx].categories = await getAchievementsCategoryById(achievements[idx].categories.join(','))
+
+	/**
+	 * Populate achievements
+	 */
+	for (const cId in achievements[idx].categories) {
+		achievements[idx].categories[cId].achievements = await getAchievementById(achievements[idx].categories[cId].achievements.join(','));
+	}
+
+	bar.increment()
 }
+
+bar.stop();
 
 fs.writeFileSync('data/achievements.json', JSON.stringify(achievements), err => {
 	if (err) {
