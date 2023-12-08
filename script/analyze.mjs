81dfb6b08d6bc65c788160b2513dd2e83f5ef709
Claudio Giordano <claudio.giordano@autistici.org>
2023-12-08 21:04:45 +0100

    Add dotenv support and auth request for getting achievement progression

--- script/analyze.mjs
+++ script/analyze.mjs
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
 
 import fs from 'fs';
+import 'dotenv/config';
 
-const data = JSON.parse(fs.readFileSync('./data/achievements.json'));
-console.log(data.length)
-const permAchis = data.filter(ach => ach.flags.includes('Permanent'));
-let acc = 0;
-//const permAchisPts = permAchis.reduce((acc, value) => console.log(value));
-console.log('perm achis', permAchis.length);
-//console.log('perm achis pts', permAchisPts);
-// console.log(achis)
+const baseURL = 'https://api.guildwars2.com/v2';
+const reqConfig = {
+	headers: {
+		'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+	}
+};
+
+const getUserProgression = async () => {
+    const resp = await fetch(`${baseURL}/account/achievements`, reqConfig);
+	return await resp.json();
+}
+
+const progression = await getUserProgression();
+console.log(progression);
+
+// const data = JSON.parse(fs.readFileSync('./data/achievements.json'));
+// console.log(data.length)
+// const permAchis = data.filter(ach => ach.flags.includes('Permanent'));
+// let acc = 0;
+// //const permAchisPts = permAchis.reduce((acc, value) => console.log(value));
+// console.log('perm achis', permAchis.length);
+// //console.log('perm achis pts', permAchisPts);
+// // console.log(achis)
