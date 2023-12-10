93e62581a2cd8829a105dd6ff9189e9c7f657b58
Claudio Giordano <claudio.giordano@autistici.org>
2023-12-10 01:05:25 +0100

    Initial points breakdown analysis

--- script/analyze.mjs
+++ script/analyze.mjs
@@ -7,17 +7,16 @@ const baseURL = 'https://api.guildwars2.com/v2';
 const reqConfig = {
 	headers: {
 		'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+		Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
 	}
 };
 
 const getUserProgression = async () => {
-    const resp = await fetch(`${baseURL}/account/achievements`, reqConfig);
+	const resp = await fetch(`${baseURL}/account/achievements`, reqConfig);
 	return await resp.json();
-}
+};
 
 const progression = await getUserProgression();
-console.log(progression);
 
 fs.writeFileSync('data/progression.json', JSON.stringify(progression), err => {
 	if (err) {
@@ -26,12 +25,32 @@ fs.writeFileSync('data/progression.json', JSON.stringify(progression), err => {
 	console.log('JSON data is saved.');
 });
 
+const achis = JSON.parse(fs.readFileSync('./data/achievements.json'));
 
-// const data = JSON.parse(fs.readFileSync('./data/achievements.json'));
-// console.log(data.length)
-// const permAchis = data.filter(ach => ach.flags.includes('Permanent'));
-// let acc = 0;
-// //const permAchisPts = permAchis.reduce((acc, value) => console.log(value));
-// console.log('perm achis', permAchis.length);
-// //console.log('perm achis pts', permAchisPts);
-// // console.log(achis)
+let tPts = 0;
+achis.forEach(group => {
+	console.log(`${group.name}: `);
+	let gPts = 0;
+	group.categories.forEach(cat => {
+		let cPts = 0;
+
+		if (!cat.achievements || cat.achievements.constructor !== Array) {
+			console.warn(`Invalid achievements data for ${cat.name}, expected array`);
+			return;
+		}
+
+		cat.achievements.forEach(ach => {
+			let aPts = ach.tiers.reduce((aPts, t) => t.points + aPts, 0);
+			// console.log(` -> ${ach.name}: ${aPts}`);
+			cPts = cPts + aPts;
+		});
+
+		console.log(` -> ${cat.name}: ${cPts}`);
+		gPts = gPts + cPts;
+	});
+
+	tPts = tPts + gPts;
+	console.log(`${group.name} total points: ${gPts}`);
+});
+
+console.log(`Total points: ${tPts}`);
