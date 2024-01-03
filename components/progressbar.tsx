c684ac8bc99fe52137c1ab59ba229e01ebc25934
Claudio Giordano <claudio.giordano@autistici.org>
2024-01-04 00:41:57 +0100

    Remove useless interface

--- components/progressbar.tsx
+++ components/progressbar.tsx
@@ -1,12 +1,10 @@
-interface Props {
-	percentage: number;
-	label: string;
-}
-const ProgressBar = ({ percentage = 0, label = 'N/A'}: Props) => {
+const ProgressBar = ({ percentage = 0, label = 'N/A' }: { percentage: number; label: string }) => {
 	return (
 		<div className="flex w-full h-4 overflow-hidden font-sans text-xs font-medium rounded-full flex-start bg-blue-gray-50">
-			<div className={`flex items-center justify-center h-full overflow-hidden text-white break-all bg-gray-900 rounded-full`}
-            style={{width: `${percentage}%` }}>
+			<div
+				className={`flex items-center justify-center h-full overflow-hidden text-white break-all bg-gray-900 rounded-full`}
+				style={{ width: `${percentage}%` }}
+			>
 				{label} {percentage}% Completed
 			</div>
 		</div>
