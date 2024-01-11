502d6433d1843d2feed647a757c9a6c80d63e5bb
Claudio Giordano <claudio.giordano@autistici.org>
2024-01-11 15:58:31 +0100

    Set env variable for next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        API_GW_BASE_URL: 'https://api.guildwars2.com/v2',
+        API_GW_BASE_OPTIONS: {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    }
+}
 
 module.exports = nextConfig
