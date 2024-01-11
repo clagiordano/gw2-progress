87bab608cce7d0286b7e71a560d87c26ffbf5675
Claudio Giordano <claudio.giordano@autistici.org>
2024-01-11 16:13:52 +0100

    Prevent error on empty progression

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -8,94 +8,99 @@ import { ProgressDetails } from './ProgressDetails';
 
 export default async function Page() {
 	async function analyze() {
-		console.log('token', process.env.ACCESS_TOKEN);
+		// console.log('token', process.env.ACCESS_TOKEN);
 		// console.log(achievements);
 
-		const progression = await getUserProgression();
+		const progression = await getUserProgression() || [];
 
 		let tPts = 0;
 		let utPts = 0;
-		achievements.map((group: any) => {
-			// console.log(`${group.name}: `);
-			let gPts = 0;
-			let ugPts = 0;
 
-			group.gPts = 0;
-			group.ugPts = 0;
+		if (achievements && achievements.constructor === Array) {
+			achievements.map((group: any) => {
+				// console.log(`${group.name}: `);
+				let gPts = 0;
+				let ugPts = 0;
 
-			group.categories.map((cat: any) => {
-				let cPts = 0;
-				let ucPts = 0;
+				group.gPts = 0;
+				group.ugPts = 0;
 
-				cat.cPts = 0;
-				cat.ucPts = 0;
+				group.categories.map((cat: any) => {
+					let cPts = 0;
+					let ucPts = 0;
 
-				if (!cat.achievements || cat.achievements.constructor !== Array) {
-					console.warn(`Invalid achievements data for ${cat.name}, expected array`);
-					return;
-				}
+					cat.cPts = 0;
+					cat.ucPts = 0;
 
-				cat.achievements.map((ach: any) => {
-					let aPts = ach.tiers.reduce((aPts: number, t: any) => t.points + aPts, 0);
-					let uaPts = 0;
+					if (!cat.achievements || cat.achievements.constructor !== Array) {
+						console.warn(`Invalid achievements data for ${cat.name}, expected array`);
+						return;
+					}
 
-					let fIdx = progression.findIndex((a: any) => a.id == ach.id);
-					if (fIdx !== -1) {
-						// console.log('fIdx', fIdx);
-						let uach = progression[fIdx];
-						progression.splice(fIdx, 1);
-						uaPts = ach.tiers
-							.filter((t: any) => t.count <= uach.current)
-							.reduce((uaPts: number, t: any) => t.points + uaPts, 0);
+					cat.achievements.map((ach: any) => {
+						let aPts = ach.tiers.reduce((aPts: number, t: any) => t.points + aPts, 0);
+						let uaPts = 0;
 
-						if (uach.repeated) {
-							/** Include repeated times */
-							// console.log('repeated ', ach.name, uach.repeated, aPts, (aPts * uach.repeated));
-							let uaPtsRepeated = aPts * uach.repeated;
-							if (uaPtsRepeated > ach.point_cap) {
-								uaPtsRepeated = ach.point_cap;
+						if (progression && progression.constructor === Array) {
+							let fIdx = progression.findIndex((a: any) => a.id == ach.id);
+							if (fIdx !== -1) {
+								// console.log('fIdx', fIdx);
+								let uach = progression[fIdx];
+								progression.splice(fIdx, 1);
+								uaPts = ach.tiers
+									.filter((t: any) => t.count <= uach.current)
+									.reduce((uaPts: number, t: any) => t.points + uaPts, 0);
+
+								if (uach.repeated) {
+									/** Include repeated times */
+									// console.log('repeated ', ach.name, uach.repeated, aPts, (aPts * uach.repeated));
+									let uaPtsRepeated = aPts * uach.repeated;
+									if (uaPtsRepeated > ach.point_cap) {
+										uaPtsRepeated = ach.point_cap;
+									}
+									uaPts = uaPts + uaPtsRepeated;
+								}
 							}
-							uaPts = uaPts + uaPtsRepeated;
 						}
-					}
 
-					if (ach.point_cap && ach.point_cap > 0) {
-						/**
-						 * For repeatable achievements use point_cap as maximum earnable amount
-						 * -1 means infinite
-						 */
-						//console.log('override aPts', aPts, '->', ach.point_cap);
-						aPts = ach.point_cap;
-					}
+						if (ach.point_cap && ach.point_cap > 0) {
+							/**
+							 * For repeatable achievements use point_cap as maximum earnable amount
+							 * -1 means infinite
+							 */
+							//console.log('override aPts', aPts, '->', ach.point_cap);
+							aPts = ach.point_cap;
+						}
 
-					ach.aPts = aPts;
-					ach.uaPts = uaPts;
+						ach.aPts = aPts;
+						ach.uaPts = uaPts;
 
-					// console.log(` -> ${ach.name}: ${uaPts} / ${aPts}`);
-					cPts = cPts + aPts;
-					ucPts = ucPts + uaPts;
+						// console.log(` -> ${ach.name}: ${uaPts} / ${aPts}`);
+						cPts = cPts + aPts;
+						ucPts = ucPts + uaPts;
 
-					cat.cPts = cPts;
-					cat.ucPts = ucPts;
+						cat.cPts = cPts;
+						cat.ucPts = ucPts;
 
-					cat.cPtsPercent = Math.round((ucPts / (cPts || 1)) * 100);
+						cat.cPtsPercent = Math.round((ucPts / (cPts || 1)) * 100);
+					});
+
+					//console.log(`  \u21B3 ${cat.name}: ${ucPts} / ${cPts}`);
+					gPts = gPts + cPts;
+					ugPts = ugPts + ucPts;
 				});
 
-				//console.log(`  \u21B3 ${cat.name}: ${ucPts} / ${cPts}`);
-				gPts = gPts + cPts;
-				ugPts = ugPts + ucPts;
+				group.gPts = gPts;
+				group.ugPts = ugPts;
+
+				tPts = tPts + gPts;
+				utPts = utPts + ugPts;
+				// console.log(`${group.name} group points: ${ugPts} / ${gPts}`);
+				//console.log(sprintf("%25s: %5d / %5d (%3d%%)", `${group.name}`, ugPts, gPts, ((ugPts / (gPts || 1)) * 100)));
+
+				group.gPtsPercent = Math.round((ugPts / (gPts || 1)) * 100);
 			});
-
-			group.gPts = gPts;
-			group.ugPts = ugPts;
-
-			tPts = tPts + gPts;
-			utPts = utPts + ugPts;
-			// console.log(`${group.name} group points: ${ugPts} / ${gPts}`);
-			//console.log(sprintf("%25s: %5d / %5d (%3d%%)", `${group.name}`, ugPts, gPts, ((ugPts / (gPts || 1)) * 100)));
-
-			group.gPtsPercent = Math.round((ugPts / (gPts || 1)) * 100);
-		});
+		}
 
 		return achievements;
 	}
