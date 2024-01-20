66a380b3dd86dd17d7f10508bc6d7a4a1464a8b2
Claudio Giordano <claudio.giordano@autistici.org>
2024-01-20 21:48:27 +0100

    Refactor account compoenent

--- app/account/AccountInfo.tsx
+++ app/account/AccountInfo.tsx
@@ -1,39 +1,80 @@
-'use client'
+"use client";
 
-import { IAccount } from '@/models/IAccount';
-import { Card, CardHeader, CardBody, Heading, Box, Stack, StackDivider, Text } from '@chakra-ui/react';
-import { AccountInfoOwner } from '@/app/account/AccountInfoOwner';
-import { AccountInfoFeatures } from './AccountInfoFeatures';
-import { AccountInfoGuilds } from './AccountInfoGuilds';
-import { Suspense } from 'react';
+import { IAccount } from "@/models/IAccount";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Heading,
+  Box,
+  Stack,
+  StackDivider,
+  Text,
+} from "@chakra-ui/react";
+import { AccountInfoOwner } from "@/app/account/AccountInfoOwner";
+import { AccountInfoFeatures } from "./AccountInfoFeatures";
+import { AccountInfoGuilds } from "./AccountInfoGuilds";
+import { Suspense, useEffect, useState } from "react";
 
-export const AccountInfo = ({ data }: { data: IAccount }) => {
-	return (
-		<Card>
-			<CardHeader>
-				<Heading size="md">Account Information</Heading>
-			</CardHeader>
+import { getAccountInfo, setToken } from "@/app/actions";
 
-			{/* <Suspense fallback="Loading..."> */}
-				<CardBody>
-					<Stack divider={<StackDivider />} spacing="4">
-						<AccountInfoOwner data={data} />
+const initialState: IAccount = {
+  id: "-",
+  name: "-",
+  age: 0,
+  last_modified: "-",
+  world: {
+    id: 0,
+    name: "-",
+    population: "-",
+  },
+  guilds: [],
+  guild_leader: [],
+  created: "-",
+  access: ["None"],
+  commander: false,
+  fractal_level: 0,
+  daily_ap: 0,
+  monthly_ap: 0,
+  wvw_rank: 0,
+  build_storage_slots: 0,
+};
 
-						<AccountInfoFeatures data={data.access} />
+export const AccountInfo = () => {
+  const [account, setAccount] = useState(initialState);
 
-						<AccountInfoGuilds data={data.guilds} />
+  useEffect(() => {
+    getAccountInfo().then((data) => {
+      setAccount(data);
+    });
+  });
 
-						<Box>
-							<Heading size="xs" textTransform="uppercase">
-								Basic statistics
-							</Heading>
-							<Text pt="2" fontSize="sm">
-								WvW rank, fractal, commander tag, recurring APs, (masteries?) etc
-							</Text>
-						</Box>
-					</Stack>
-				</CardBody>
-			{/* </Suspense> */}
-		</Card>
-	);
+  return (
+    <Card>
+      <CardHeader>
+        <Heading size="md">Account Information</Heading>
+      </CardHeader>
+
+      {/* <Suspense fallback="Loading..."> */}
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          <AccountInfoOwner data={account} />
+
+          <AccountInfoFeatures data={account.access} />
+
+          <AccountInfoGuilds data={account.guilds} />
+
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Basic statistics
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              WvW rank, fractal, commander tag, recurring APs, (masteries?) etc
+            </Text>
+          </Box>
+        </Stack>
+      </CardBody>
+      {/* </Suspense> */}
+    </Card>
+  );
 };
--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,47 +1,10 @@
-import { getAccountInfo, setToken } from '@/app/actions';
-import { AccountInfo } from './AccountInfo';
-import { Spinner } from '@chakra-ui/react';
-import { IAccount } from '@/models/IAccount';
-import { Suspense } from 'react';
-import { AccountForm } from './AccountForm';
+import { AccountInfo } from "./AccountInfo";
+import { Suspense, useEffect, useState } from "react";
 
-const initialState: IAccount = {
-	id: '-',
-	name: '-',
-	age: 0,
-	last_modified: '-',
-	world: {
-		id: 0,
-		name: '-',
-		population: '-'
-	},
-	guilds: [],
-	guild_leader: [],
-	created: '-',
-	access: ['None'],
-	commander: false,
-	fractal_level: 0,
-	daily_ap: 0,
-	monthly_ap: 0,
-	wvw_rank: 0,
-	build_storage_slots: 0
-};
-
-let accountData: IAccount = initialState;
-
-export default function Page() {
-	async function saveToken(formData: FormData) {
-	  'use server'
-
-
-	  await setToken(formData.get('accessToken') as string);
-	  accountData = await getAccountInfo();
-	}
-
-	return <form action={saveToken}>
-		<AccountForm />
-		{/* <Suspense fallback={<Spinner size='xl' />}> */}
-			<AccountInfo data={accountData} />
-		{/* </Suspense> */}
-	</form>
-  }
+export default function Account() {
+  return (
+    <Suspense>
+      <AccountInfo />
+    </Suspense>
+  );
+}
