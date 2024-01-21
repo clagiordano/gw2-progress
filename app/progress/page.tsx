48022f52a1367105173763ee44eca17f0055f5d7
Claudio Giordano <claudio.giordano@autistici.org>
2024-01-21 01:29:48 +0100

    Refactor progress page to use state

--- app/progress/page.tsx
+++ app/progress/page.tsx
@@ -1,24 +1,28 @@
-// import achievements from '@/data/achievements.json' assert { type: 'json' };
+'use client';
+
 import { Divider } from '@chakra-ui/react';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { ProgressStats } from './ProgressStats';
 import { ProgressDetails } from './ProgressDetails';
 import { analyze } from '@/services/achievements';
-import { GetStaticProps } from 'next';
 
-export default async function Page(  ) {
-	const data = await analyze();
+export default function Progress(  ) {
+	const [progress, setProgress] = useState([]);
+
+	useEffect(() => {
+		analyze().then((data) => setProgress(data));
+	}, []);
 
 	return (
 		<div>
 			<Suspense fallback="Loading stats...">
-				<ProgressStats data={data} />
+				<ProgressStats data={progress} />
 			</Suspense>
 
 			<Divider />
 
 			<Suspense fallback="Loading details...">
-				<ProgressDetails data={data} />
+				<ProgressDetails data={progress} />
 			</Suspense>
 		</div>
 	);
