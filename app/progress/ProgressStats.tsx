5539dd2d99b2da615d451d80a56e761021dfd716
Claudio Giordano <claudio.giordano@autistici.org>
2024-01-21 04:59:43 +0100

    Refactor ProgressStats component

--- app/progress/ProgressStats.tsx
+++ app/progress/ProgressStats.tsx
@@ -1,36 +1,54 @@
 "use client";
 import {
-  StatGroup,
-  Stat,
-  StatLabel,
-  StatNumber,
-  StatHelpText,
-  StatArrow,
-  Stack,
-  SimpleGrid,
+  CircularProgress,
+  CircularProgressLabel,
+  Card,
+  CardBody,
+  CardFooter,
+  Text,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
-import { Suspense } from "react";
+
+const getColor = (value: number) => {
+  if (value <= 25) {
+    return "red.400";
+  }
+
+  if (value > 25 && value <= 75) {
+    return "yellow.400";
+  }
+
+  return "green.400";
+};
 
 export const ProgressStats = async ({ data }: { data: any[] }) => {
   return (
-	<div>
-    {/* <Suspense fallback="loading stats component"> */}
-      <StatGroup>
-        <SimpleGrid columns={8} spacing={4}>
-          {data.map((group: any) => {
-            return (
-              <Stat key={group.id}>
-                <StatLabel>{group.name}</StatLabel>
-                <StatNumber>{group.gPtsPercent}%</StatNumber>
-                <StatHelpText>
-                  {group.ugPts} / {group.gPts}
-                </StatHelpText>
-              </Stat>
-            );
-          })}
-        </SimpleGrid>
-      </StatGroup>
-    {/* </Suspense> */}
-	  </div>
+    <Wrap spacing={4}>
+      {data.map((group: any) => {
+        return (
+          <WrapItem key={group.id}>
+            <Card h={150} w={220}>
+              <CardBody p={1} textAlign={"center"}>
+                <CircularProgress
+                  value={group.gPtsPercent}
+                  color={getColor(group.gPtsPercent)}
+                  key={group.id}
+                  size={"100px"}
+                >
+                  <CircularProgressLabel>
+                    {group.gPtsPercent}%
+                  </CircularProgressLabel>
+                </CircularProgress>
+              </CardBody>
+
+              <CardFooter p={1} textAlign={"center"}>
+                <Text w={"100%"}>{group.name}</Text>
+              </CardFooter>
+            </Card>
+          </WrapItem>
+        );
+      })}
+    </Wrap>
   );
 };
