db97008689acb3bd65c0eb0a0a539cb5b0c69f3d
Claudio Giordano <claudio.giordano@autistici.org>
2025-11-13 02:23:29 +0100

    Update get by id route

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -1,16 +1,21 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import itemsRaw from "@/app/lib/items.json";
 import { Item } from "@/models/item";
 
 const items: Item[] = itemsRaw as Item[];
 
-export async function GET(_: Request, { params }: { params: { id: string } }) {
-  const id = parseInt(params.id, 10);
-  const item = items.find(i => i.id === id);
+export const GET = async (
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) => {
+  const { id } = await context.params;
+  const itemId = parseInt(id, 10);
+
+  const item = items.find((i) => i.id === itemId);
 
   if (!item) {
     return NextResponse.json({ error: "Item not found" }, { status: 404 });
   }
 
   return NextResponse.json(item);
-}
+};
