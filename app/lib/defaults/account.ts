5d7cf4f88aeb5ab9fa50ffbf041ecdf21229d6db
Claudio Giordano <claudio.giordano@autistici.org>
2025-11-16 21:43:50 +0100

    Centralize Account initial state and protect from failures

---
+++ app/lib/defaults/account.ts
@@ -0,0 +1,23 @@
+import { Account } from "@/models/account";
+
+export const initialAccountState: Account = {
+  id: "-",
+  name: "-",
+  age: 0,
+  last_modified: "-",
+  world: {
+    id: 0,
+    name: "-",
+    population: "-",
+  },
+  guilds: [],
+  guild_leader: [],
+  created: "-",
+  access: ["None"],
+  commander: false,
+  fractal_level: 0,
+  daily_ap: 0,
+  monthly_ap: 0,
+  wvw_rank: 0,
+  build_storage_slots: 0,
+};
--- models/account.ts
+++ models/account.ts
@@ -1,4 +1,4 @@
-export interface IAccount {
+export interface Account {
 	id: string;
 	age: number;
 	name: string;
