97862580eaba6536885f8804dbca85a738b07b27
Claudio Giordano <claudio.giordano@autistici.org>
2025-11-22 15:30:19 +0100

    Tune response size

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        API_GW_BASE_URL: 'https://api.guildwars2.com/v2',
+  env: {
+    API_GW_BASE_URL: "https://api.guildwars2.com/v2",
+  },
+  images: {
+    domains: ["render.guildwars2.com"],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
     },
-    images: {
-        domains: ['render.guildwars2.com'],
-    },
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
