74b1dee8b8dbc19c604ec9c943ba5e2551450095
Claudio Giordano <claudio.giordano@autistici.org>
2025-11-23 03:14:39 +0100

    Fix legacy AchievementGroup

--- components/AchievementGroup.tsx
+++ components/AchievementGroup.tsx
@@ -1,25 +1,18 @@
 import { Group } from "@/models/achievement";
-import { getColor } from "@/services/utils";
 import { AccordionItem, AccordionButton, AccordionIcon, Box, Progress, AccordionPanel, Text } from "@chakra-ui/react";
+import { ProgressBar } from "./ProgressBar";
 
 export default function AchievementGroup({ group }: { group: Group }) {
   return (
     <AccordionItem key={group.id}>
       <AccordionButton>
         <AccordionIcon />
-        <Box flex="1" textAlign="left">
-          <Text fontSize="xl" fontWeight="bold">
-            {group.name}
-          </Text>
-          <Progress
-            mt={2}
-            value={group.gPtsPercent}
-            colorScheme={getColor(group.gPtsPercent)}
-          />
-          <Text fontSize="sm" color="gray.500">
-            {group.ugPts} / {group.gPts} points
-          </Text>
-        </Box>
+        <ProgressBar
+          percentage={group.gPtsPercent}
+          label={group.name}
+          currentPoints={group.ugPts}
+          totalPoints={group.gPts}
+        />
       </AccordionButton>
 
       <AccordionPanel pb={4}>
