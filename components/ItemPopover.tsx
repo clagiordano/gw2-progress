842a5fdfcc795c9bc58b55e3fd76ed4669b31e4f
Claudio Giordano <claudio.giordano@autistici.org>
2025-11-29 15:57:29 +0100

    Explore items: Optimize performance for popover

--- components/ItemPopover.tsx
+++ components/ItemPopover.tsx
@@ -12,7 +12,7 @@ import {
   Portal,
   PortalProps
 } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { memo, ReactNode } from "react";
 import { Item } from "@/models/item";
 
 interface ItemPopoverProps {
@@ -20,39 +20,37 @@ interface ItemPopoverProps {
   children: ReactNode;
   portalProps?: PortalProps;
 }
+const ItemPopoverComponent = ({ item, children, portalProps }: ItemPopoverProps) => (
+  <Popover trigger="hover" placement="top-start" openDelay={150} closeDelay={200} isLazy>
+    <PopoverTrigger>
+      <Box>{children}</Box>
+    </PopoverTrigger>
 
-export default function ItemPopover({ item, children, portalProps }: ItemPopoverProps) {
-  return (
-    <Popover trigger="hover" placement="top-start" openDelay={150} closeDelay={200}>
-      <PopoverTrigger>
-        <Box>{children}</Box>
-      </PopoverTrigger>
+    <Portal {...portalProps}>
+      <PopoverContent w="auto" maxW="sm">
+        <PopoverArrow />
+        <PopoverBody>
+          <VStack align="start" spacing={2}>
+            <Text fontWeight="bold">{item.name ?? "N/A"}</Text>
+            <Text>Type: {item.type ?? "N/A"}</Text>
+            {item.details?.type && <Text>Subtype: {item.details.type}</Text>}
+            {item.details?.bonuses && (
+              <Box>
+                <Text fontWeight="bold">Bonuses:</Text>
+                <VStack align="start" spacing={0}>
+                  {item.details.bonuses.map((bonus) => (
+                    <Text key={bonus} fontSize="sm">
+                      {bonus}
+                    </Text>
+                  ))}
+                </VStack>
+              </Box>
+            )}
+          </VStack>
+        </PopoverBody>
+      </PopoverContent>
+    </Portal>
+  </Popover>
+);
 
-      <Portal {...portalProps}>
-        <PopoverContent w="auto" maxW="sm" pointerEvents="none">
-          <PopoverArrow />
-          <PopoverBody>
-            <VStack align="start" spacing={2}>
-              <Text fontWeight="bold">{item.name ?? "N/A"}</Text>
-              <Text>Type: {item.type ?? "N/A"}</Text>
-              {item.details?.type && <Text>Subtype: {item.details.type}</Text>}
-
-              {item.details?.bonuses && (
-                <Box>
-                  <Text fontWeight="bold">Bonuses:</Text>
-                  <VStack align="start" spacing={0}>
-                    {item.details.bonuses.map((b, i) => (
-                      <Text key={i} fontSize="sm">
-                        {b}
-                      </Text>
-                    ))}
-                  </VStack>
-                </Box>
-              )}
-            </VStack>
-          </PopoverBody>
-        </PopoverContent>
-      </Portal>
-    </Popover>
-  );
-}
+export default memo(ItemPopoverComponent);
